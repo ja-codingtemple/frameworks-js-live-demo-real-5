8510d824d7a283da2f59f2b068778b338334e1d4
Jeremy <jeremya@codingtemple.com>
2025-05-06 09:52:17 -0600

    Starter code from the live lesson.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function changeText() {
+    let title = document.getElementById("title");
+    title.innerHTML = "This title was changed by JavaScript!";
+}
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+
+add(5, 10);
